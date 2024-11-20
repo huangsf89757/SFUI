1983a421aacc1dc34b0ca33687bf814ef1ead0c4
hsf89757 <hsf89757@gmail.com>
2024-11-20 09:27:39 +0800

    优化内存

--- SFUI/Classes/Resource/SFImage.swift
+++ SFUI/Classes/Resource/SFImage.swift
@@ -19,35 +19,35 @@ extension SFImage {
             UIImage.sf.image(name: name, bundle: Self.bundle)
         }
         
-        public static var close: UIImage? = image(name: "close")
-        public static var back: UIImage? = image(name: "back")
+        public static var close: UIImage? { image(name: "close") }
+        public static var back: UIImage? { image(name: "back") }
         public struct Doc {
-            public static var folder: UIImage? = image(name: "doc/folder")
-            public static var file: UIImage? = image(name: "doc/file")
+            public static var folder: UIImage? { image(name: "doc/folder") }
+            public static var file: UIImage? { image(name: "doc/file") }
         }
         public struct Arrow {
-            public static var top: UIImage? = image(name: "arrow/top")
-            public static var left: UIImage? = image(name: "arrow/left")
-            public static var right: UIImage? = image(name: "arrow/right")
-            public static var bottom: UIImage? = image(name: "arrow/bottom")
+            public static var top: UIImage? { image(name: "arrow/top") }
+            public static var left: UIImage? { image(name: "arrow/left") }
+            public static var right: UIImage? { image(name: "arrow/right") }
+            public static var bottom: UIImage? { image(name: "arrow/bottom") }
         }
         public struct Triangle {
-            public static var top: UIImage? = image(name: "triangle/top")
-            public static var left: UIImage? = image(name: "triangle/left")
-            public static var right: UIImage? = image(name: "triangle/right")
-            public static var bottom: UIImage? = image(name: "triangle/bottom")
+            public static var top: UIImage? { image(name: "triangle/top") }
+            public static var left: UIImage? { image(name: "triangle/left") }
+            public static var right: UIImage? { image(name: "triangle/right") }
+            public static var bottom: UIImage? { image(name: "triangle/bottom") }
         }
         public struct Checkbox {
-            public static var nor: UIImage? = image(name: "checkbox/nor")
-            public static var sel: UIImage? = image(name: "checkbox/sel")
+            public static var nor: UIImage? { image(name: "checkbox/nor") }
+            public static var sel: UIImage? { image(name: "checkbox/sel") }
         }
         public struct Hud {
-            public static var loading: UIImage? = image(name: "hud/loading")
-            public static var success: UIImage? = image(name: "hud/success")
-            public static var failure: UIImage? = image(name: "hud/failure")
-            public static var info: UIImage? = image(name: "hud/info")
-            public static var warning: UIImage? = image(name: "hud/warning")
-            public static var error: UIImage? = image(name: "hud/error")
+            public static var loading: UIImage? { image(name: "hud/loading") }
+            public static var success: UIImage? { image(name: "hud/success") }
+            public static var failure: UIImage? { image(name: "hud/failure") }
+            public static var info: UIImage? { image(name: "hud/info") }
+            public static var warning: UIImage? { image(name: "hud/warning") }
+            public static var error: UIImage? { image(name: "hud/error") }
         }
     }
 }
