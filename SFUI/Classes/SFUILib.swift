fbe2adb247967035904d50e5427941c1c63ab2cd
hsf89757 <hsf89757@gmail.com>
2024-12-11 11:45:25 +0800

    fix

--- SFUI/Classes/SFUILib.swift
+++ SFUI/Classes/SFUILib.swift
@@ -6,11 +6,27 @@
 //
 
 import Foundation
+import UIKit
 // Basic
 import SFExtension
+// Third
+import IQKeyboardManagerSwift
 
 // MARK: - SFUILib
 public class SFUILib: SFLib {
     public static var bundle: Bundle? = Bundle.sf.bundle(cls: SFUILib.self, resource: nil)
 }
 
+
+// TODO: 换个位置
+extension UIView {
+    public func addToolbarPreviousNextAllowedClassIfNot() {
+        var cls = IQKeyboardManager.shared.toolbarPreviousNextAllowedClasses
+        let isContains = cls.contains(where: { ele in
+            ele is Self
+        })
+        if !isContains {
+            cls.append(Self.self)
+        }
+    }
+}
